7ae0e7af7cde070055e7686ff7b641f2e7214bd2
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-05-17 08:26:43 +0000

    create function added Professeur

--- controllers/ParentsController.php
+++ controllers/ParentsController.php
@@ -1,53 +1,58 @@
 <?php
+
 namespace controllers;
 
 use models\Parente;
 
-class ParentsController{
+class ParentsController
+{
 
-    public function listParents(){
-        $data=Parente::getAll();
-        view('parents_list',true,['data'=>$data]);
+    public function listParents()
+    {
+        $data = Parente::getAll();
+        view('parents_list', true, ['data' => $data]);
     }
-  
+
     public function addFormParent()
     {
-       view('formaddparente',true);
+        view('formaddparente', true);
     }
 
-    public function addParentSave(){
+    public function addParentSave()
+    {
         $p1 = new Parente();
-        $p1->matricule=$_POST['matricule'];
-        $p1->nom_complet=$_POST['nom_complet'];
-        $p1->genre=$_POST['genre'];
-        $p1->job=$_POST['job'];
-        $p1->adresse=$_POST['adresse'];
-        $p1->phone=$_POST['phone'];
+        $p1->matricule = $_POST['matricule'];
+        $p1->nom_complet = $_POST['nom_complet'];
+        $p1->genre = $_POST['genre'];
+        $p1->job = $_POST['job'];
+        $p1->adresse = $_POST['adresse'];
+        $p1->phone = $_POST['phone'];
         $p1->save();
         redirect('parents');
-        
     }
 
-    public function delete(){
-        $d=Parente::getById($_GET['id']);
-        if($d){
+    public function delete()
+    {
+        $d = Parente::getById($_GET['id']);
+        if ($d) {
             $d->delete();
             redirect('parents');
-        }else 
+        } else
             echo 'parente not existe';
     }
 
-    public function formEdit(){
-        $obj=Parente::getById($_GET['id']);
-        view('formupdateparent',true,['par'=>$obj]);
+    public function formEdit()
+    {
+        $obj = Parente::getById($_GET['id']);
+        view('formupdateparent', true, ['par' => $obj]);
     }
 
     // public function update(){
     //     $p=Parente::getById($_POST['id']);
-      
+
     //     $p->save();
     // }
-    
+
 
 
 }
--- controllers/ProfesseursController.php
+++ controllers/ProfesseursController.php
@@ -15,4 +15,16 @@ class ProfesseursController
   {
     view('AddProfesseur', true);
   }
+  public function AddProfesseurSave()
+  {
+    $Prof = new ProfesseursController();
+    $Prof->Matricule = $_POST['Matricule'];
+    $Prof->Nom_complet = $_POST['Nom_complet'];
+    $Prof->Genre = $_POST['Genre'];
+    $Prof->Class_id  = $_POST['Class_id '];
+    $Prof->Matiere = $_POST['Matiere'];
+    $Prof->Phone = $_POST['Phone'];
+    $Prof->save();
+    redirect('Professeur');
+  }
 }
