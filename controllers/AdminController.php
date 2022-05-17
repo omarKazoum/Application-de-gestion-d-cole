fb39c9258c3f6d166740925c11ecdf4ad7f70f01
achaayb <achaaybdev@gmail.com>
2022-05-17 13:56:41 +0200

    admin(rewrite) rewrite based on EasyBaseModel

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,6 +6,7 @@ use utils\SessionManager;
 require_once '../autoloader.php';
 class AdminController
 {
+
     public function initLogin(){
         view('login',false, array());
     }
@@ -13,8 +14,9 @@ class AdminController
         session_start();
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $bar = Admin::getByEmail($email);
-        if ($bar && password_verify($password,$bar->PasswordHash())){
+        $bar = Admin::getBy('email', $email);
+        var_dump($bar);
+        if ($bar && password_verify($password, password_hash($bar[0]->passwordHash, PASSWORD_DEFAULT))){
             $_SESSION['admin_obs'] = $bar;
             redirect("admin/crud");
         }
@@ -29,14 +31,14 @@ class AdminController
         $foo->nom = $_POST['nom'];
         $foo->prenom = $_POST['prenom'];
         $foo->email = $_POST['email'];
-        $foo->setPassword($_POST['password']);
+        $foo->password = $_POST['password'];
         $foo->role = $_POST['role'];
         $foo->add();
         redirect("admin/crud");
     }
     public function delete(){
         $foo = new Admin();
-        $foo->setId($_POST['admin-id']);
+        $foo->id = $_POST['admin-id'];
         $foo->delete();
         redirect("admin/crud");
     }
@@ -46,7 +48,7 @@ class AdminController
         $foo->nom = $_POST['nom'];
         $foo->prenom = $_POST['prenom'];
         $foo->email = $_POST['email'];
-        $foo->setPassword($_POST['password']);
+        $foo->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $foo->role = $_POST['role'];
         $foo->update();
         redirect("admin/crud");
--- models/Admin.php
+++ models/Admin.php
@@ -4,15 +4,18 @@ namespace models;
 
 use utils\Constants;
 
-class Admin extends BaseModel
+class Admin extends EasyBaseModel
 {
     //it's necessary to set the table name
-    protected static string $tableName= 'admins';
+    protected static string $tableName = "admins";
+    protected static array $columns = ['nom','prenom','role','password','email'];
+    protected static string $idColumnName='id';
+    public $id;
     public $nom;
     public $prenom;
     public $role;
     public string $email;
-    private string $passwordHash;
+    public string $passwordHash;
 
     /**
      * @param string $passwordHash
@@ -25,7 +28,7 @@ class Admin extends BaseModel
     /**
      * @return string
      */
-    public function     PasswordHash(): string
+    public function PasswordHash(): string
     {
         return $this->passwordHash;
     }
@@ -39,54 +42,15 @@ class Admin extends BaseModel
         $this->passwordHash = password_hash($password, PASSWORD_DEFAULT);
     }
 
-    public function update()
-    {
-        $sql="UPDATE ".self::$tableName." SET "
-            ."nom".'=?,'
-            ."prenom".'=?,'
-            ."email".'=?,'
-            ."role".'=?,'
-            ."password"."=? ".
-            'WHERE '.self::ID_KEY.'=?';
-        $stmt =self::$db_manager->getConnection()->prepare($sql);
-
-        $stmt->bind_param( "ssssss",$this->nom,$this->prenom,$this->email,$this->role,$this->passwordHash,$this->id);
-        $stmt->execute();
-        return $stmt->affected_rows;
-    }
-
-    public function add()
-    {
-        $sql="INSERT INTO ".self::$tableName."("
-            ."nom".','
-            ."prenom".','
-            ."role".','
-            ."email".','
-            ."password".")"
-            ."values(?,?,?,?,?)";
-        $stmt =self::$db_manager->getConnection()->prepare($sql);
-        $stmt->bind_param( "sssss",$this->nom,$this->prenom,$this->role,$this->email,$this->passwordHash);
-        $stmt->execute();
-        $this->id = $stmt->insert_id;
-        return $stmt->get_result();
-    }
-
     protected static function parseEntity(array $data)
     {
         $user = new Admin();
-        $user->setId($data["id"]);
+        $user->id=$data["id"];
         $user->email =$data["email"];
         $user->setPasswordHash($data["password"]);
         $user->nom = $data["nom"];
         $user->prenom = $data["prenom"];
         $user->role = $data["role"];
         return $user;
-    }
-    public static function getByEmail($name)
-    {
-        $res = self::queryBy("email", $name, self::$tableName);
-        if ($res)
-            return static::parseEntity($res[0]);
-        return false;
     }
 }
