f0cd600cc2d49eb0145dd30c5ca955bd698f8ece
omar kazoum <omarKazoum96@gmail.com>
2022-05-19 15:59:58 +0100

    fixed a bug in classes list

--- views/classes/list.php
+++ views/classes/list.php
@@ -1,27 +1,34 @@
-<div class="d-flex justify-content-between">
-  <h3 class="mt-2">Classes list <?= isset($isSearching) ? 'results of ' . $_GET['word'] : '' ?></h3>
+<div class="d-flex justify-content-between border-bottom-1 mb-2">
+  <h3 class="">Classes list <?= isset($isSearching) ? 'results of "' . $_GET['word'].'"' : '' ?></h3>
   <a href="<?= getUrlFor('classes/add') ?>" class="btn btn-primary"> add class</a>
 </div>
 
 <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Description</th>
-      <th scope="col" colspan="2"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php //if (!$classes) { ?>
-    <div class="alert alert-danger">
-      no classes found!
-    </div>
 
-    <?php //} else
-      //foreach ($classes as $class) {
-        //TODO:: [] array problem
-    ?>
-    <?php //endif ?>
+  <tbody>
+    <?php if (!$classes) { ?>
+            <div class="alert alert-danger">
+                no classes found! clcik <a href="<?= getUrlFor('classes/add')?>" class="lick text-decoration-none">here</a> to add a class
+            </div>
+
+    <?php } else{?>
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col" ></th>
+    </tr>
+    </thead>
+    <?php  foreach ($classes as $class) {?>
+          <tr>
+              <th scope="row"><?= $class->id?></th>
+              <td><?= $class->name?></td>
+              <td><?= $class->description?></td>
+              <td><a href="<?= getUrlFor('classes/delete/'.$class->id) ?>" class="btn btn-danger">Delete</a><a href="<?= getUrlFor('classes/edit/'.$class->id)?>" class="btn btn-success ms-1">Edit</a></td>
+          </tr>
+    <?php }
+
+    }?>
   </tbody>
 </table>
