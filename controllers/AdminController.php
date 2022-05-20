870239f7f168855c09f8acf2b036838620a49c07
achaayb <achaaybdev@gmail.com>
2022-05-20 11:25:00 +0200

    feature(admin) authentication protection

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -11,12 +11,11 @@ class AdminController
         view('login',false, array());
     }
     public function verifyLogin(){
-        session_start();
         $email = $_POST['email'];
         $password = $_POST['password'];
         $bar = Admin::getBy('email', $email);
         if (!empty($bar) && password_verify($password, $bar[0]->passwordHash)){
-            $_SESSION['admin_obs'] = $bar;
+            SessionManager::getInstance()->login($bar[0]->id);
             redirect("admin");
         }
         view('login',false, array("err" => true));
