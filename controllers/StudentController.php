335d976bf1973e3b21e3f196f02ea340e74d4d00
mohammed <mohammedwanir67@gmail.com>
2022-05-20 15:20:45 +0100

    authentication partie etudiant

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -20,18 +20,21 @@ class StudentController{
     }
 
     public function SaveStudent(){
-        $s1 = new Student();
-        $s1->matricule=$_POST['matricule'];
-        $s1->name=$_POST['name'];
-        $s1->gender=$_POST['gender'];
-        $s1->id_class=$_POST['id_class'];
-        $s1->id_parents=$_POST['id_parents'];
-        $s1->adresse=$_POST['adresse'];
-        $s1->birthday=$_POST['birthday'];
-        $s1->email=$_POST['email'];
-        $s1->parent_name=$_POST['parent_name'];
-        $s1->save();
-        redirect('student');
+        if(!empty($_POST['matricule']) || !empty($_POST['name'])||!empty($_POST['gender'])||!empty($_POST['id_class'])||!empty($_POST['id_parents'])||!empty($_POST['adresse'])||!empty($_POST['birthday'])||!empty($_POST['email'])||!empty($_POST['parent_name'])){
+            $s1 = new Student();
+            $s1->matricule=$_POST['matricule'];
+            $s1->name=$_POST['name'];
+            $s1->gender=$_POST['gender'];
+            $s1->id_class=$_POST['id_class'];
+            $s1->id_parents=$_POST['id_parents'];
+            $s1->adresse=$_POST['adresse'];
+            $s1->birthday=$_POST['birthday'];
+            $s1->email=$_POST['email'];
+            $s1->parent_name=$_POST['parent_name'];
+            $s1->save();
+            redirect('student');
+        }
+       
         
     }
 
@@ -50,6 +53,7 @@ class StudentController{
     }
 
      public function update(){
+        if(!empty($_POST['matricule']) || !empty($_POST['name'])||!empty($_POST['gender'])||!empty($_POST['id_class'])||!empty($_POST['id_parents'])||!empty($_POST['adresse'])||!empty($_POST['birthday'])||!empty($_POST['email'])||!empty($_POST['parent_name'])){
         $s1=student::getById($_POST['id']);
         $s1->matricule=$_POST['matricule'];
         $s1->name=$_POST['name'];
@@ -61,7 +65,7 @@ class StudentController{
         $s1->email=$_POST['email'];
         $s1->parent_name=$_POST['parent_name'];
         $s1->save();
-        redirect('student');
+        redirect('student');}
      }
     
 
